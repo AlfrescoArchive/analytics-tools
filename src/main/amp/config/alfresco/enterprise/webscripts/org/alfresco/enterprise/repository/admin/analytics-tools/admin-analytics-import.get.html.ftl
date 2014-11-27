231d71affef3913f803c5d179329c49e619f44b1
Gethin James <gethin@covulucion-350V5C>
2014-11-27 14:38:16 +0100

    Initial generated archetype

---
+++ src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-import.get.html.ftl
@@ -0,0 +1,9 @@
+<#include "../admin-template.ftl" />
+<@page title="Analytics Admin Import">
+   <div class="column-left">
+      <@section label="Some Values" />
+   </div>
+   <div class="column-right">
+      <@section label="More Values" />
+   </div>
+</@page>
---
+++ src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-import.get.js
@@ -0,0 +1,9 @@
+<import resource="classpath:alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/admin-common.lib.js">
+
+/**
+ * Repository Admin Console
+ * 
+ * Analytics tools
+ */
+model.tools = Admin.getConsoleTools("admin-analytics-tools");
+model.metadata = Admin.getServerMetaData();
