2559d6d581dac4b8f7a41f23903770231f8a0b8d
Gethin James <gethin@covulucion-350V5C>
2014-11-27 15:28:24 +0100

    Removed import scripts for now

--- src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-import.get.html.ftl
+++
@@ -1,9 +0,0 @@
-<#include "../admin-template.ftl" />
-<@page title="Analytics Admin Import">
-   <div class="column-left">
-      <@section label="Some Values" />
-   </div>
-   <div class="column-right">
-      <@section label="More Values" />
-   </div>
-</@page>
--- src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-import.get.js
+++
@@ -1,9 +0,0 @@
-<import resource="classpath:alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/admin-common.lib.js">
-
-/**
- * Repository Admin Console
- * 
- * Analytics tools
- */
-model.tools = Admin.getConsoleTools("admin-analytics-tools");
-model.metadata = Admin.getServerMetaData();
