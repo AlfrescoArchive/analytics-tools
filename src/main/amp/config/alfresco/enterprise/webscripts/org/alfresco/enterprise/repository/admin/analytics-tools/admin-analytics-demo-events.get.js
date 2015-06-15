6a4d93369ad7617ca0136e0a059f9b0d97f604c6
Gethin James <gethin@covulucion-350V5C>
2015-06-15 11:46:04 +0200

    Returns a maximum of 500 sites.

--- src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-demo-events.get.js
+++ src/main/amp/config/alfresco/enterprise/webscripts/org/alfresco/enterprise/repository/admin/analytics-tools/admin-analytics-demo-events.get.js
@@ -6,5 +6,5 @@
  * Analytics tools
  */
 model.tools = Admin.getConsoleTools("admin-analytics-demo-events");
-model.sitesList = siteService.listSites(null, null);
+model.sitesList = siteService.listSites(null, null, 500);
 model.people = groups.getGroup("ANALYTICS_DEMO_USERS").getAllUsers();
